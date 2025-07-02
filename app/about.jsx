1181e613b9dae7112cf2461ae0f85de1553320ad
Gregory Hudler <gregory.hudler@outlook.com>
2025-07-02 01:02:19 -0400

    Added new screens and top bar back navigation

---
+++ app/about.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router'
+
+const About = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>About Kin-Kitchen</Text>
+      <Text style={{textAlign: 'center'}}>How may times have you wished you could remember your grandmothers special recipie?  How many of you have children about to set off on their own for the first time and you want to give them the taste of home?  How many friends do you have that always ask how you made this dish?
+      </Text>
+      <Text style={{textAlign: 'center'}}>Kin-Kitchen is a mobile application platform desinged to help chefs from all walks of life preserve their delicious recipies to share with the world.</Text>
+      <Text style={{textAlign: 'center'}}>A key feature of our application is the ability to create personalized cook books that can be ordered directly from a POD service so that you can easily create a beautiful and professional cook book with all your family recipies.</Text>
+    </View>
+  )
+}
+export default About
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img:
+  {
+    marginVertical: 20,
+  },
+   link: 
+   {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  }
+})
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/images/Kin-Kitchen.png'
+import { Link } from 'expo-router'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img}/>
+      <Text style={styles.title}>Where Family Recipies live forever.</Text>
+      <Link href="/about" style={styles.link}>About</Link>
+      <Link href="/Contact" style={styles.link}>Contact Us</Link>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img:
+  {
+    marginVertical: 20,
+  },
+  link: 
+  {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  }
+})
