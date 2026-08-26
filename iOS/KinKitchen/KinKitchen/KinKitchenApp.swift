1ac71704ae0ce605ae9bc59bf05f4cb84a957dc8
Gregory Hudler <gregory.hudler@outlook.com>
2026-08-26 14:20:09 -0400

    Project creation

---
+++ iOS/KinKitchen/KinKitchen/KinKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinKitchenApp.swift
+//  KinKitchen
+//
+//  Created by Greg Hudler on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct KinKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/KinKitchen/KinKitchenTests/KinKitchenTests.swift
@@ -0,0 +1,19 @@
+//
+//  KinKitchenTests.swift
+//  KinKitchenTests
+//
+//  Created by Greg Hudler on 8/26/26.
+//
+
+import Testing
+@testable import KinKitchen
+
+struct KinKitchenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
